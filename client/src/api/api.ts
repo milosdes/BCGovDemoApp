830a33e2e92523869dedd9bd68ba718e387199ff
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-04-26 20:41:32 -0700

    Create build with prod server

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:
-        process.env.baseURL ||
-        'http://143.198.42.69' ||
-        'http://localhost:3000/api',
+    baseURL: 'http://143.198.42.69/api',
 });
 
 export default api;
