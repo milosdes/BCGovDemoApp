e9da4117bd56e62064e2ecd87c9cfed2fee177c4
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-04-26 20:33:41 -0700

    Change baseURL for api

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.baseURL || 'http://localhost:3000/api',
+    baseURL:
+        process.env.baseURL ||
+        'http://143.198.42.69' ||
+        'http://localhost:3000/api',
 });
 
 export default api;
